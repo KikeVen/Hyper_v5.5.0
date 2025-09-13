c894f6736960c74072900a7aed946c821235d1fe
Enrique Bruzual <bruzual.kike@gmail.com>
2025-09-13 18:41:08 -0400

    update to assets

--- Stater-Kit/src/assets/js/pages/demo.tasks.js
+++ Stater-Kit/src/assets/js/pages/demo.tasks.js
@@ -1,13 +1,1 @@
-/*
-Template Name: Ubold - Responsive Bootstrap 5 Admin Dashboard
-Author: CoderThemes
-Website: https://coderthemes.com/
-Contact: support@coderthemes.com
-File: Quilljs init js
-*/
-
-
-// Bubble theme
-var quill = new Quill('#bubble-editor', {
-    theme: 'bubble'
-});
+var quill=new Quill("#bubble-editor",{theme:"bubble"});
